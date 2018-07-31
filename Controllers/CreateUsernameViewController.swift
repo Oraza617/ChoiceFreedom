757890df0cbfc22fecff58fbf818be0ba287e01a
Owais Raza <owaisraza64@gmail.com>
2018-07-31 16:41:43 -0700

    Login and Username View Controllers

---
+++ Controllers/CreateUsernameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateUsername.swift
+//  ChoiceFreedom
+//
+//  Created by Owais Raza on 7/31/18.
+//  Copyright © 2018 Makeschool. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateUsernameViewController: UIViewController {
+    
+    //Textfield and next button
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    
+    //View controller life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //When "next" is tapped
+    @IBAction func nextButtonTapped(_ sender: Any) {
+    }
+    
+
+}
