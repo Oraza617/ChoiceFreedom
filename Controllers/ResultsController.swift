67f4ebe8f9d825931af4391a9a37e197b457cd1e
araza617 <araza617@gmail.com>
2019-08-16 20:37:22 -0500

    Added % display to the results

--- Controllers/ResultsController.swift
+++ Controllers/ResultsController.swift
@@ -42,8 +42,25 @@ class ResultsController: UIViewController, UITableViewDataSource, UITableViewDel
         let imageTwourl = URL(string: entry.imageTwoURL)
         cell.imageToTheRight.kf.setImage(with: imageTwourl)
         cell.questionLabel.text = entry.question
-        cell.voteCountLeftImage.text = String(entry.imageOneCounter)
-        cell.voteCountRightImage.text = String(entry.imageTwoCounter)
+        
+        let totalImageCounts = entry.imageOneCounter + entry.imageTwoCounter
+        
+        // Calculate the % if the both pictures have a vote count that's greater than 0, otherwise just display the numbers
+        if (entry.imageOneCounter > 0 && entry.imageTwoCounter > 0) {
+            let leftImagePercentage = (Double(entry.imageOneCounter) / Double(totalImageCounts)) * 100
+            let rightImagePercentage = ((Double(entry.imageTwoCounter) / Double(totalImageCounts)) * 100)
+            
+            // Properly format to only display the numerical % and no values after the decimal
+            cell.voteCountLeftImage.text = String(format: "%.f", leftImagePercentage) + "% (" + String(entry.imageOneCounter) + ")"
+            cell.voteCountRightImage.text = String(format: "%.f", rightImagePercentage) + "% (" + String(entry.imageTwoCounter) + ")"
+        }
+        
+        // Only displaying the numbers if either of the counts are not greater than 0
+        else {
+            cell.voteCountLeftImage.text = String(entry.imageOneCounter)
+            cell.voteCountRightImage.text = String(entry.imageTwoCounter)
+        }
+        
         cell.setGradientForResultTableViewBackground(colorOne: Colors.kindaOwaisBlue, colorTwo: Colors.kindaOwaisBlue)
         cell.selectionStyle = UITableViewCellSelectionStyle.none
         return cell
